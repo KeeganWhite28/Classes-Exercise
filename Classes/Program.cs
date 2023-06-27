bd2d688bd694b1a1f6fbd03e9810011542170bdf
Keegan White <keegan28white@gmail.com>
2023-06-27 11:31:31 -0500

    initial commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            var carList = new List<Car>() { myCar };
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+
         }
     }
 }
